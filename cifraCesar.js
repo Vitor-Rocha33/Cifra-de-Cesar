6ef5a3ee5498bd5573c69a748c951afd1a832245
VitorRochaMaqEstagio <vitor.rocha.adm@gmail.com>
2019-08-22 17:46:33 -0300

    Adicionando inputs

---
+++ cifraCesar.js
@@ -0,0 +1,1 @@
+window.cifraCesar = new cifraDeCesar();
---
+++ scripts/cifraCesarController.js
@@ -0,0 +1,26 @@
+class cifraDeCesar {
+
+    anteriorLetra(){
+    var c =s.charCodeAt(0);
+
+        switch(c) {
+            case 12: return '~';
+            default: return String.fromCharCode(-cc);
+        }
+    }
+
+
+    proximaLetra(){
+    var c = s.charCodeAt(0);
+
+        switch(c) {
+            case 126: return ' ';
+            default: return String.fromCharCode(++c);
+        }
+    }
+
+    decifrar(){
+    let valor = document.getElementById('staticText');
+    }
+
+}
