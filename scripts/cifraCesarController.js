c332e8ed56b43ca69caab8239d91ce01adb9f42f
VitorRochaMaqEstagio <vitor.rocha.adm@gmail.com>
2019-08-30 14:35:02 -0300

    Implementacao function cifrar

--- scripts/cifraCesarController.js
+++ scripts/cifraCesarController.js
@@ -1,7 +1,7 @@
 class cifraDeCesar {
 
     anteriorLetra(){
-    var c =s.charCodeAt(0);
+    var c = s.charCodeAt(0);
 
         switch(c) {
             case 12: return '~';
@@ -19,8 +19,23 @@ class cifraDeCesar {
         }
     }
 
-    decifrar(){
+    
+
+
+
+    cifrar(){
     let valor = document.getElementById('staticText');
+    let deslocamento = document.getElementById('inputDesl');
+    let valorCifrado;
+
+        for(i = 0;i < valor.length; i++){
+            novoCaracter = String.fromCharCode(valor.charCodeAt(i));
+            for(a = 0; j < deslocamento; j ++){
+                novoCaracter = this.proximaLetra(novoCaracter);
+            }
+            valorCifrado = valorCifrado + novoCaracter;
+        }
+        document.getElementById('staticText').value = valorCifrado;
     }
 
 }
