4273c08f860fb128898aa118af1990fbc8840227
Vitor Rocha Morais <vitor.rocha.adm@gmail.com>
2019-09-06 14:38:49 -0300

    Projeto Final Cifra de Cesar

--- scripts/cifraCesarController.js
+++ scripts/cifraCesarController.js
@@ -1,16 +1,15 @@
-class cifraDeCesar {
 
-    anteriorLetra(){
-    var c =s.charCodeAt(0);
+    function previousLetter(s){
+    var c = s.charCodeAt(0);
 
         switch(c) {
             case 12: return '~';
-            default: return String.fromCharCode(-cc);
+            default: return String.fromCharCode(--c);
         }
     }
 
 
-    proximaLetra(){
+    function nextLetter(s){
     var c = s.charCodeAt(0);
 
         switch(c) {
@@ -19,8 +18,43 @@ class cifraDeCesar {
         }
     }
 
-    decifrar(){
-    let valor = document.getElementById('staticText');
+    function decrypt(){
+        var value = $("#staticText").val();
+        var displacement = $("#inputDisplacement").val();
+        var valueDecrypted = "";
+
+        for (i = 0; i < value.length; i++){
+            var newLetter = String.fromCharCode(value.charCodeAt(i));
+
+            for (a = 0; a < displacement; a++){                
+                newLetter = previousLetter(newLetter);
+            }
+
+            valueDecrypted = valueDecrypted + newLetter;
+        }
+
+        document.getElementById('staticText').value = valueDecrypted;
+
     }
 
-}
+    function encrypt(){
+    var value = $("#staticText").val();
+    var displacement = $("#inputDisplacement").val();
+    var valueEncrypted = '';
+
+    for (i = 0; i < value.length; i++){
+        let newLetter = String.fromCharCode(value.charCodeAt(i));
+
+        for(a = 0; a < displacement; a++){
+            newLetter = nextLetter(newLetter);
+        }
+
+        valueEncrypted = valueEncrypted + newLetter;
+
+    }
+
+    document.getElementById('staticText').value = valueEncrypted;
+    
+    }
+
+
